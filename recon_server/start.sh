86a37a9095f2c58f3a73e607cf55061282b7433f
J <jlove7714@gmail.com>
2026-07-17 06:45:11 -0400

    left my start.sh in there...

--- recon_server/start.sh
+++
@@ -1,8 +0,0 @@
-if [ $# -ne 1 ]; then
-	echo "Enter a project path"
-	exit 1
-fi
-
-project="$1"
-
-python3 recon_server.py --host 192.168.122.75 --port 5000 "$project" --bh-key 'NUrXqg43trOfVLNCkNb0DX/763IKWRPJMSZq0oUmJXMhgGkTJ8yNRA==' --bh-key-id 'df9f1cd9-26b9-419b-b8c0-d9425474a066' --bh-host 'http://192.168.122.75:8880' 
